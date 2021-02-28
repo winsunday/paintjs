243a95b0bd86db89146f7e51fc659c0f2de596bc
winsunday <70320230+winsunday@users.noreply.github.com>
2021-02-28 14:52:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("controls__color jsColor");
-const range = document.getElementById("jsRange")
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const INITIAL_COLOR = "#2c2c2c"
+const saveBtn = document.getElementById("jsSave");
+// const CANVAS_SIZE = ?   *캔버스가 정사각형이 아니라 하나로 정의하지 못함
 
 canvas.width = 700;
 canvas.height = 600;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 700, 600) 
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -36,12 +44,38 @@ function onMouseUp(event){
 }
 
 function handleColorClick(event){
-    console.log(event.target.style);
-}
-
-function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+   const size = event.target.value;
+   ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Paint"
+    }else{
+        filling = true;
+        mode.innerText = "Fill"
+    }
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0, 0, 700, 600)
+    }
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJs[🎨]";
+    link.click();
 }
 
 if(canvas){
@@ -49,6 +83,19 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+if(range){
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
